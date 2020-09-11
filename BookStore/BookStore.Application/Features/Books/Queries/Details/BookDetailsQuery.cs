1a0a5d92b5d7d1ec86b73b1aac0d6e65a0eb6e0a
vdimov <vdimov@objectsystems.com>
2020-09-11 14:00:01 +0300

    new projects added

---
+++ BookStore/BookStore.Application/Features/Books/Queries/Details/BookDetailsQuery.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Application.Features.Books.Queries.Details
+{
+    class BookDetailsQuery
+    {
+    }
+}
